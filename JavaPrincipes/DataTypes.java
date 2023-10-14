16d13c8ec769a3df775993a3f863ae1d255ef169
MakoskiVictor <makoski.ed@gmail.com>
2023-10-14 08:07:45 -0300

    add some data types

---
+++ JavaPrincipes/DataTypes.java
@@ -0,0 +1,42 @@
+package JavaPrincipes;
+
+public class DataTypes {
+
+    // PRIMITIVOS
+    
+    // ENTEROS -> CUENTAN CON SUBDIVISIÓN:
+    
+        // Int -> desde -2.147.483.648 hasta 2.147.483.467 (4 bytes) main
+    
+        int salario = 2147483647;
+    
+        // Short -> desde -32.768 hasta 32.767 (2 bytes)
+
+        short longitud = 32767;
+    
+        // Long -> (8 bytes) Al final hay que poner una L
+
+        long habitantes = 5000000000000L;
+    
+        // Byte -> desde -128 hasta 127 (1 byte)
+
+        byte edad = 78;
+    
+    // DECIMALES (Números flotantes)
+    
+        // Float -> de 6 a 7 cifras decimales (4 bytes). Sufijo F
+
+        float precio = 7.68F;
+    
+        // Double -> hasta 15 cifras decimales (8 bytes) main
+
+        double PI = 3.141516;
+    
+    // CHAR -> representa caracteres como 'a'
+
+        char index = 'a';
+    
+    // BOOLEAN
+
+        boolean registered = true;
+}
